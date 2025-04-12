119c3c1e6343c99d664e31c160baf7a23f7194b4
alegom05 <146905500+alegom05@users.noreply.github.com>
2025-04-11 20:47:13 -0500

    vista principal y vista de cuenta de taxi listos, falta terminar la barra inferior. Redireccion a la vista de cuenta.

--- Proyecto/app/src/main/java/com/example/hotroid/TaxiActivity.java
+++ Proyecto/app/src/main/java/com/example/hotroid/TaxiActivity.java
@@ -1,13 +1,29 @@
 package com.example.hotroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class TaxiActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.taxi_main);
+
+
+        CardView cardTaxista = findViewById(R.id.cardTaxista);
+        cardTaxista.setOnClickListener(v -> {
+            Intent intent = new Intent(TaxiActivity.this, TaxiCuenta.class);
+            startActivity(intent);
+        });
+
+
+
+
+
     }
 }
---
+++ Proyecto/app/src/main/java/com/example/hotroid/TaxiCuenta.java
@@ -0,0 +1,23 @@
+package com.example.hotroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class TaxiCuenta extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.taxi_cuenta);
+
+
+
+
+
+
+
+
+    }
+}
