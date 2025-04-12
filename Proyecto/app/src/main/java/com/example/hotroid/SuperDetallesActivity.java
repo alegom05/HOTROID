6a681e635175da0186fa4fac90af0dab9622d93a
Pedro Bustamante <a20206156@pucp.edu.pe>
2025-04-11 21:25:00 -0500

    reservas telo
    reservas telo

--- Proyecto/app/src/main/java/com/example/hotroid/SuperDetallesActivity.java
+++ Proyecto/app/src/main/java/com/example/hotroid/SuperDetallesActivity.java
@@ -1,14 +1,28 @@
 package com.example.hotroid;
-
+import android.widget.Button;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+
+
+
 public class SuperDetallesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_super_detalles);
+        setContentView(R.layout.super_detalles_hotel);
+
+        Button btnVerReservas = findViewById(R.id.btnVerReservas);
+        btnVerReservas.setOnClickListener(v -> {
+            Intent intent = new Intent(SuperDetallesActivity.this, SuperReservasActivity.class);
+            startActivity(intent);
+        });
+    }
+
+
+
+
 }
--- Proyecto/app/src/main/java/com/example/hotroid/SuperReservasActivity.java
+++ Proyecto/app/src/main/java/com/example/hotroid/SuperReservasActivity.java
@@ -1,4 +1,14 @@
 package com.example.hotroid;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 public class SuperReservasActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.super_reservas_hotel);
+
+
+    }
 }
