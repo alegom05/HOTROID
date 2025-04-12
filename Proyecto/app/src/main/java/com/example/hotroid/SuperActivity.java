12a86220249e3c38db03706c3877effad3ee6416
Pedro Bustamante <a20206156@pucp.edu.pe>
2025-04-11 19:18:08 -0500

    superadmin hotel

--- Proyecto/app/src/main/java/com/example/hotroid/SuperActivity.java
+++ Proyecto/app/src/main/java/com/example/hotroid/SuperActivity.java
@@ -1,13 +1,25 @@
 package com.example.hotroid;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class SuperActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.super_main);
+
+        // Ejemplo para un Hotel (debes hacerlo para cada hotel)
+        CardView cardAranwa = findViewById(R.id.cardHotel1); // Asegúrate que este ID coincida con tu XML
+        cardAranwa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SuperActivity.this, SuperDetallesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Proyecto/app/src/main/java/com/example/hotroid/SuperReservasActivity.java
@@ -0,0 +1,4 @@
+package com.example.hotroid;
+import androidx.appcompat.app.AppCompatActivity;
+public class SuperReservasActivity extends AppCompatActivity{
+}
