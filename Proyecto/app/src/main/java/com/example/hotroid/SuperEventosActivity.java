cbc7827da6a655c81d584eac14756257284d2a8e
Pedro Bustamante <a20206156@pucp.edu.pe>
2025-04-13 17:20:26 -0500

    eventos 1

---
+++ Proyecto/app/src/main/java/com/example/hotroid/SuperEventosActivity.java
@@ -0,0 +1,13 @@
+package com.example.hotroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuperEventosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.super_reservas_hotel);
+    }
+}
