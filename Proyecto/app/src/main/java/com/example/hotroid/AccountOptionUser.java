6caa6a88c29965783f55cace22ac1dd8050faf05
NikolMontes <a20203644@pucp.edu.pe>
2025-04-13 00:25:59 -0500

    nueva vista_user

---
+++ Proyecto/app/src/main/java/com/example/hotroid/AccountOptionUser.java
@@ -0,0 +1,22 @@
+package com.example.hotroid;
+
+import android.os.Bundle;
+import com.example.hotroid.R;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AccountOptionUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.user_account_option);
+
+
+    }
+}
--- Proyecto/app/src/main/java/com/example/hotroid/ClienteActivity.java
+++ Proyecto/app/src/main/java/com/example/hotroid/ClienteActivity.java
@@ -1,13 +1,42 @@
 package com.example.hotroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hotroid.databinding.ActivityMainBinding;
+import com.example.hotroid.databinding.ClienteMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ClienteActivity extends AppCompatActivity {
+
+    private ClienteMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cliente_main);
+
+        binding = ClienteMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item ->{
+        int itemId = item.getItemId();
+        if (itemId ==R.id.nav_hoteles_user){
+            startActivity(new Intent(ClienteActivity.this, MainActivity.class));
+            return true;
+        } else if (itemId == R.id.nav_reservas_user) {
+            startActivity(new Intent(ClienteActivity.this, MainActivity.class));
+            return true;
+        } else if (itemId == R.id.nav_chat_user) {
+            startActivity(new Intent(ClienteActivity.this , MainActivity.class));
+            return true;
+        } else if (itemId == R.id.nav_cuenta) {
+            startActivity(new Intent(ClienteActivity.this, AccountOptionUser.class));
+            return true;
+        }
+        return false;
+        });
     }
 }
