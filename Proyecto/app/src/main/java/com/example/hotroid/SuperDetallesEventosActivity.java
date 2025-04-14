53cc1f0cdd837b571a667cb39dfc364dc12f98ab
Pedro Bustamante <a20206156@pucp.edu.pe>
2025-04-13 23:16:02 -0500

    eventos superadmin

---
+++ Proyecto/app/src/main/java/com/example/hotroid/SuperDetallesEventosActivity.java
@@ -0,0 +1,14 @@
+package com.example.hotroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuperDetallesEventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.super_detalles_eventos);
+    }
+}
