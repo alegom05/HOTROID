f164f8387a9aa3d7157046076ed4c69cf979db6b
alegom05 <146905500+alegom05@users.noreply.github.com>
2025-04-13 20:30:50 -0500

    Barra Inferior implementada en la vista principal

--- Proyecto/app/src/main/java/com/example/hotroid/TaxiActivity.java
+++ Proyecto/app/src/main/java/com/example/hotroid/TaxiActivity.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.example.hotroid.databinding.ClienteMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class TaxiActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.taxi_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
 
         CardView cardTaxista = findViewById(R.id.cardTaxista);
@@ -29,7 +38,24 @@ public class TaxiActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-
+        // BottomNavigationView o Barra inferior de menú
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.wifi) {
+                Intent intentInicio = new Intent(TaxiActivity.this, TaxiCuenta.class);
+                startActivity(intentInicio);
+                return true;
+            } else if (item.getItemId() == R.id.location) {
+                Intent intentUbicacion = new Intent(TaxiActivity.this, TaxiAlertas.class);
+                startActivity(intentUbicacion);
+                return true;
+            } else if (item.getItemId() == R.id.notify) {
+                Intent intentAlertas = new Intent(TaxiActivity.this, TaxiAlertas.class);
+                startActivity(intentAlertas);
+                return true;
+            } else {
+                return false;
+            }
+        });
 
 
 
