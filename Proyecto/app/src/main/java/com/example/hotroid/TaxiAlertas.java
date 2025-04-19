2d51fd116214050522a290e9dd4a6a24a6e9d7bd
alegom05 <146905500+alegom05@users.noreply.github.com>
2025-04-19 11:43:43 -0500

    Todas las vistas Parte 2

--- Proyecto/app/src/main/java/com/example/hotroid/TaxiAlertas.java
+++ Proyecto/app/src/main/java/com/example/hotroid/TaxiAlertas.java
@@ -15,27 +15,26 @@ public class TaxiAlertas extends AppCompatActivity {
 
 
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
 
 
         // BottomNavigationView o Barra inferior de menú
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.wifi) {
-                Intent intentInicio = new Intent(TaxiAlertas.this, TaxiActivity.class);
-                startActivity(intentInicio);
                 return true;
-            } else if (item.getItemId() == R.id.location) {
+            }
+            else if (item.getItemId() == R.id.location) {
                 Intent intentUbicacion = new Intent(TaxiAlertas.this, TaxiLocation.class);
                 startActivity(intentUbicacion);
                 return true;
-            } else if (item.getItemId() == R.id.notify) {
+            }
+            else if (item.getItemId() == R.id.notify) {
                 Intent intentAlertas = new Intent(TaxiAlertas.this, TaxiAlertas.class);
                 startActivity(intentAlertas);
                 return true;
-            } else {
-                return false;
             }
+            return false; // Devuelve false si no se seleccionó ningún ítem válido
         });
 
 
