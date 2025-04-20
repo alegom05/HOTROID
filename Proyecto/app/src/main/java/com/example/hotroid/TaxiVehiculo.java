c5e8f8c48360bbc2ed84a4806a5237f787198896
alegom05 <146905500+alegom05@users.noreply.github.com>
2025-04-19 23:21:29 -0500

    Avances en taxiauto

---
+++ Proyecto/app/src/main/java/com/example/hotroid/TaxiVehiculo.java
@@ -0,0 +1,40 @@
+package com.example.hotroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class TaxiVehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.taxi_vehiculo);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.wifi) {
+                Intent intentInicio = new Intent(TaxiVehiculo.this, TaxiActivity.class);
+                startActivity(intentInicio);
+                return true;
+            }
+            else if (item.getItemId() == R.id.location) {
+                Intent intentUbicacion = new Intent(TaxiVehiculo.this, TaxiLocation.class);
+                startActivity(intentUbicacion);
+                return true;
+            }
+            else if (item.getItemId() == R.id.notify) {
+                Intent intentAlertas = new Intent(TaxiVehiculo.this, TaxiAlertas.class);
+                startActivity(intentAlertas);
+                return true;
+            }
+            return false; // Devuelve false si no se seleccionó ningún ítem válido
+        });
+
+
+    }
+}
