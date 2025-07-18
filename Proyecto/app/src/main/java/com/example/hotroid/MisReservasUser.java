de399d1fabd6fcfa9e68294853557eb09e08950c
IsaacAHS <isaac.huamani@pucp.edu.pe>
2025-07-18 15:50:47 -0500

    menu reserva user corregido

--- Proyecto/app/src/main/java/com/example/hotroid/MisReservasUser.java
+++ Proyecto/app/src/main/java/com/example/hotroid/MisReservasUser.java
@@ -48,7 +48,7 @@ public class MisReservasUser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        /*EdgeToEdge.enable(this);*/
         setContentView(R.layout.user_mis_reservas);
 
         // Inicializar Firebase
